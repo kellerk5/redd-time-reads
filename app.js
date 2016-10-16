182a83591f4151f4e6c13af518e27b2aaa3f7c83
Kyle Keller <kellerk5@miamioh.edu>
2016-10-16 14:20:13 -0500

    Small syntax adjustments

--- app.js
+++ app.js
@@ -12,27 +12,27 @@ CronJob = require('cron').CronJob,
 twilioOutBoundNumber = '+15005550006';
 
 let textJob = new CronJob({
-  //10:00 PM = '00 00 22 * * 1-7'
-  cronTime: '00 11-15 21 * * 1-7',
-  onTick: function() {
-    //scraping sub-Reddits
-    scraper.grabRedditInfo();
+    //10:00 PM = '00 00 22 * * 1-7'
+    cronTime: '00 00 22 * * 1-7',
+    onTick: function() {
+        //scraping sub-Reddits
+        scraper.grabRedditInfo();
 
-    //message object sent to phone
-    client.messages.create({
-      body: 'Hello from Node',
-      to: '+13306973345',  
-      from: twilioOutBoundNumber 
-    }, function(err, message) {
-      if (err) {
-        console.log('error yo! ', err);
-      } else {
-        console.log('testing yo!');
-      }
-    });
-  },
-  start: false,
-  timeZone: 'America/Chicago'
+        //message object sent to phone
+        client.messages.create({
+            body: 'Hello from Node',
+            to: '+13306973345',  
+            from: twilioOutBoundNumber 
+        }, function(err, message) {
+            if (err) {
+                console.log('error yo! ', err);
+            } else {
+                console.log('testing yo!');
+            }
+        });
+    },
+    start: false,
+    timeZone: 'America/Chicago'
 });
 
 // test log
